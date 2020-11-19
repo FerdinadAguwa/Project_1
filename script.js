71b3fe68cfea236ebae62c36bfe6ab7dc5e23eac
N8 <nwscott81@gmail.com>
2020-11-18 21:25:38 -0600

    initial push didn't contain everything I was working on

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 /* Notes section for how I want to pull things.
-on the button press for each genre card, take the ID of the card to pass into the API call. ON the response break down each song into it's parts:
-1. Artist
-2. Song
+on the button press for each genre card, take the ID of the card to pass into the iTunesApiCall. 
+ON the response each songs' following info and stick it into an array:
+1. Artist: response.results.songs.data.attributes.artistName
+2. SongTitle: response.results.songs.data.attributes.name
+
+iterrate the array into the list where it shows up as Artits - Song Name
 
 On clicking one of the songs in the list, on the right side the Lyrics (https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime)
-will appear along with the Album cover (itunes API)
+will appear along with the Album cover (iTunes API/ response.results.songs.data.attributes.artwork.url
 
 Rock
 R&B
@@ -13,4 +16,14 @@ Classic Rock
 Alternative Rock
 Rap
 Metal
+Pop
+
 */
+
+// Variables
+var artistName = "";
+var songName = "";
+var lyricsApiCall = "https://api.lyrics.ovh/v1/" + artistName +"/" + songName;
+var iTunesApiCall = "https://api.music.apple.com/v1/catalog/{storefront}/charts?types=songs&genre=" + /* genreID */ + "&limit=10";
+
+// 
