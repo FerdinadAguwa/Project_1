8933559b250df4a9a768e79bd51aaddf349bf617
N8 <nwscott81@gmail.com>
2020-11-18 21:23:18 -0600

    my initial code upload, mostly psudocoding to get the API's written down.

---
+++ script.js
@@ -0,0 +1,16 @@
+/* Notes section for how I want to pull things.
+on the button press for each genre card, take the ID of the card to pass into the API call. ON the response break down each song into it's parts:
+1. Artist
+2. Song
+
+On clicking one of the songs in the list, on the right side the Lyrics (https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime)
+will appear along with the Album cover (itunes API)
+
+Rock
+R&B
+Classical
+Classic Rock
+Alternative Rock
+Rap
+Metal
+*/
